1606bf9afe400b175aafe9c3bc6c5deea6233445
seunglee <seoungleelee@gmail.com>
2022-05-26 19:42:24 +0900

    js commit.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c"; 
+ctx.lineWidth = 2.5;
 
 let painting = false;
+
+function startPainting(){
+    painting = true;
+}
 
 function stopPainting(){
     painting = false;
@@ -9,19 +17,22 @@ function stopPainting(){
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event){
     painting = true;
 }
 
-function onMouseUp(event){
-    stopPainting();
-}
-
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", onMouseUp);
     canvas.addEventListener("mouseleave", stopPainting);
 }
