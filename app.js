8f274ff70438234ec11cb0830f6175a1747201aa
seunglee <dlrpwkd@naver.com>
2022-05-29 12:00:50 +0900

    fillRect

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const colors = document.querySelectorAll(".jsColor");
 const range = document.querySelector("#jsRange");
 const mode = document.querySelector("#jsMode");
 
+const INITIAL_COLOR = "2c2c2c"
+const CANVAS_SIZE = 700;
+
 canvas.width = 700;
 canvas.height = 700;
 
-ctx.strokeStyle = "#2c2c2c"; 
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR; 
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -36,6 +40,7 @@ function onMouseMove(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleModeClick(){
@@ -48,11 +53,18 @@ function handleModeClick(){
     }
 }
 
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
 function handleRangChange(event){
